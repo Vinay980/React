d60e32ee9b8f015b06fde25d87e3e4fed5cafc52
Vinay <vinay00mahto@gmail.com>
2025-05-30 23:59:33 +0530

    color

--- 04-bgChanger/src/App.jsx
+++ 04-bgChanger/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState("olive")
+  const [color, setColor] = useState("red")
 
   return (
     <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
